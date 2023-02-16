e1bdc32ef36588886db6753f936ba95c2d7abc40
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-02-16 15:42:19 +0100

    do icons

--- Only_Rare_Video/src/components/CustomFooter.jsx
+++ Only_Rare_Video/src/components/CustomFooter.jsx
@@ -9,26 +9,28 @@ const CustomFooter = () => {
         <div className="col-6 pt-2 pb-2  text-white text-decoration-none text-center cssFontFooter">
           All rights reserved © 2023 Only Rare Video
         </div>
-        <div className="col-6 px-5 d-flex justify-content-end align-items-center">
+        <div className="col-6  d-flex justify-content-end align-items-center">
           <div className="text-white text-decoration-none text-center cssFontFooter ">
             Follow us on:
           </div>
-          <div className="d-flex ">
-            <img
-              className="img-fluid p-1 px-2 "
-              src="src\logo\facebook_icon.png"
-              alt="facebook"
-            />
-            <img
-              className="img-fluid p-1  px-2"
-              src="src\logo\instagram_icon.png"
-              alt=""
-            />
-            <img
-              className="img-fluid p-1 px-2"
-              src="src\logo\twitter_icon.png"
-              alt=""
-            />
+          <div className="container">
+            <div className="row justify-content-center">
+              <img
+                className="col-1 img-fluid p-1 px-2 "
+                src="src\logo\facebook_icon.png"
+                alt="facebook"
+              />
+              <img
+                className="col-1 img-fluid p-1  px-2"
+                src="src\logo\instagram_icon.png"
+                alt=""
+              />
+              <img
+                className="col-1 img-fluid p-1 px-2"
+                src="src\logo\twitter_icon.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </section>
