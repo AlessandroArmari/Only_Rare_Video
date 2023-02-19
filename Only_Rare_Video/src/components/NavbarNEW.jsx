c9417638392abc185b9974f218435f1432e79990
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-02-19 16:08:50 +0100

    navbar finally done

--- Only_Rare_Video/src/components/NavbarNEW.jsx
+++ Only_Rare_Video/src/components/NavbarNEW.jsx
@@ -1,61 +1,46 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Logo from "./Logo";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 Logo;
 const NavbarNEW = () => {
   return (
     <section>
-      <nav className="p-0 navbar navbar-expand-sm  bg-black cssFontNavbarClass">
-        <Logo></Logo>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      {/* +++++++++++++++++++++++++ */}
 
-        <div className="collapse navbar-collapse  " id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active mx-4 ">
-              <NavLink
-                className=" nav-link text-white text-decoration-none text-center cssFontNavbarClass"
-                to="/about"
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item active mx-4">
-              <NavLink
-                className="nav-link text-white text-decoration-none text-center cssFontNavbarClass"
-                to="/collection"
-              >
-                Collection
-              </NavLink>
-            </li>
-            <li className="nav-item active mx-3 ">
-              <NavLink
-                className="nav-link text-white text-decoration-none text-center cssFontNavbarClass"
-                to="/comingsoon"
-              >
-                Out Soon
-              </NavLink>
-            </li>
-            <li className="nav-item active mx-4">
-              <NavLink
-                className="nav-link text-white text-decoration-none text-center cssFontNavbarClass"
-                to="/newsletter"
-              >
-                NewsLetter
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar
+        className="p-0 navbar navbar-expand-lg   cssFontNavbarClass"
+        collapseOnSelect
+        expand="lg"
+        bg="black"
+        variant="dark"
+      >
+        <Container>
+          <Logo></Logo>
+          <Navbar.Brand></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto ">
+              <Nav.Link className="navLinkHover" href="/about">
+                ABOUT
+              </Nav.Link>
+              <Nav.Link className="navLinkHover" href="/collection">
+                COLLECTION
+              </Nav.Link>
+              <Nav.Link className="navLinkHover" href="/comingsoon">
+                OUT SOON
+              </Nav.Link>
+              <Nav.Link className="navLinkHover" href="/newsletter">
+                NEWSLETTER
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
       <div>
         <Outlet />
       </div>
